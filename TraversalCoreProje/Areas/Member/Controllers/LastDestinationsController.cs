07d816f85c20a21e2fc2a73845ee9bbae9140ca1
MuratYucedag <yucedagmurat23@gmail.com>
2023-04-05 10:44:25 +0300

    Ders 95 sonu

---
+++ TraversalCoreProje/Areas/Member/Controllers/LastDestinationsController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class LastDestinationsController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+        public LastDestinationsController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+        public IActionResult Index()
+        {
+            var values = _destinationService.TGetLast4Destinations();
+            return View(values);
+        }
+    }
+}
