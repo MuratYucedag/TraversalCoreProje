4e4d8d75deabe5f8ac95d92c2a02ba784eda6836
MuratYucedag <yucedagmurat23@gmail.com>
2023-04-10 12:49:08 +0300

    Ders 97 sonu

--- TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
-    [AllowAnonymous]
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
@@ -18,6 +18,16 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
         }
     }
 }
